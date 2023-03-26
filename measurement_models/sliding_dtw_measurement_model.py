fd9b28d07de34bc9e7ca256efd53a525be6b0fd3
Christian Frieß <Ch.Friess@campus.lmu.de>
2023-03-26 22:21:57 +0200

    added sliding dtw measurement model

---
+++ measurement_models/sliding_dtw_measurement_model.py
@@ -0,0 +1,26 @@
+from tslearn.metrics import dtw
+from strategies.measurement_strategy import MeasurementStrategy
+from utils.particle_reference_retriever import ParticleReferenceRetriever
+from utils.particle_set import ParticleSet
+
+
+class SlidingDTWMeasurementModel(MeasurementStrategy):
+
+    def __init__(self, reference_signal: list):
+        super().__init__()
+        self.reference_signal = reference_signal
+        self.measurement_history = []
+        self.particle_reference_retriever = ParticleReferenceRetriever()
+
+    def raw_weight_particles(self, particles: ParticleSet, measurement: float) -> ParticleSet:
+        self.measurement_history.append(measurement)
+        if len(self.measurement_history) > 10:
+            self.measurement_history = self.measurement_history[-10:]
+
+        for particle in particles:
+            particle.reference_history += self.particle_reference_retriever.retrieve_reference_update(
+                particle, self.reference_signal)
+            if len(particle.reference_history) > 10:
+                particle.reference_history = particle.reference_history[-10:]
+            particle.weight = dtw(self.measurement_history, particle.reference_history)
+        return particles
