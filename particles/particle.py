a1a186a4c22222c6555ea3c4bd34debddcb6f145
Christian Frieß <Ch.Friess@campus.lmu.de>
2023-06-12 21:00:43 +0200

    first sketch of 3D particle reference retriever

--- particles/particle.py
+++ particles/particle.py
@@ -50,7 +50,47 @@ class AlphaMapParticle(Particle):
         self.map = [state.alpha for _ in range(len1Dreference)]
 
 
+class SlidingParticle(Particle):
+
+    def __init__(self,
+                 state: State,
+                 weight: float = 0) -> None:
+        super().__init__(state, weight)
+        self.reference_history = []
+        self.last_reference_index = 0
+
+    def __str__(self):
+        return f'[State: {str(self.state)} | Weight: {self.weight} | ReferenceHistory: {str(self.reference_history)}]'
+
+    def __eq__(self, other):
+        if isinstance(other, SlidingParticle):
+            return self.state == other.state \
+                and self.weight == other.weight \
+                and self.reference_history == other.reference_history
+        return False
 
 
+class SlidingParticle3D(Particle3D):
+    def __init__(self,
+                 state: State3D,
+                 weight: float = 0) -> None:
+        super().__init__(state, weight)
+        self.reference_history = []
+        self.last_reference_index = {"branch_index": 0, "displacement_index": 0}
 
+    def __str__(self):
+        return f'[State: {str(self.state)} | Weight: {self.weight} | ReferenceHistory: {str(self.reference_history)}]'
 
+    def __eq__(self, other):
+        if isinstance(other, SlidingParticle):
+            return self.state == other.state \
+                and self.weight == other.weight \
+                and self.reference_history == other.reference_history
+        return False
+
+    def get_last_reference_index(self) -> dict:
+        return self.last_reference_index
+
+    def set_last_reference_index(self, branch_index: int, displacement_index: int):
+        self.last_reference_index["branch_index"] = branch_index
+        self.last_reference_index["displacement_index"] = displacement_index
--- utils/particle_reference_retriever.py
+++ utils/particle_reference_retriever.py
@@ -11,7 +11,7 @@ class ParticleReferenceRetriever:
             return [reference_signal[0]]
         elif position > len(reference_signal) - 1:
             particle.last_reference_index = len(reference_signal)-1
-            return [reference_signal[len(reference_signal) - 1]]
+            return [reference_signal[- 1]]
         # forward motion
         if particle.last_reference_index <= position:
             reference_update = self.retrieve_reference_update_forward(
@@ -52,4 +52,62 @@ class ParticleReferenceRetriever:
 
 class ParticleReferenceRetriever3D:
     def retrieve_reference_update(self, particle, map3D: Map3D) -> list:
-        pass
+        current_branch = particle.get_state().get_position()["branch"]
+        current_displacement = round(particle.get_state().get_position()["displacement"])
+        last_branch_index = particle.get_last_reference_index()["branch_index"]
+        last_displacement_index = particle.get_last_reference_index()["displacement_index"]
+
+        reference_update = []
+
+        if current_displacement < 0:
+            particle.set_last_reference_index(branch_index=current_branch, displacement_index=0)
+            reference_update = [map3D.get_vessel(0)[0]]
+        elif current_displacement > len(map3D.get_vessel(current_branch)) - 1:
+            particle.set_last_reference_index(branch_index=current_branch,
+                                              displacement_index=len(map3D.get_vessel(current_branch)) - 1)
+            reference_update = [map3D.get_vessel(current_branch)[-1]]
+
+        # forward motion in same branch
+        elif last_displacement_index <= current_displacement and last_branch_index == current_branch:
+            for index in range(last_displacement_index + 1, current_displacement + 1):
+                if index < len(map3D.get_vessel(current_branch)):
+                    reference_update.append(map3D.get_vessel(current_branch)[index])
+
+        # forward motion with branch switch
+        elif last_branch_index < current_branch:
+            reference_update = map3D.get_vessel(last_branch_index)[last_displacement_index:]
+            last_branch_index += 1
+            while last_branch_index < current_branch:
+                reference_update += map3D.get_vessel(last_branch_index)
+                last_branch_index += 1
+            for index in range(current_displacement):
+                if index < len(map3D.get_vessel(current_branch)):
+                    reference_update.append(map3D.get_vessel(current_branch)[index])
+
+        # backward motion in same branch
+        elif last_displacement_index > current_displacement and last_branch_index == current_branch:
+            for index in range(last_displacement_index - 1, current_displacement - 1, -1):
+                if 0 <= index:
+                    reference_update.append(map3D.get_vessel(current_branch)[index])
+        # backward motion in other branch
+        elif last_branch_index > current_branch:
+            reference_update = map3D.get_vessel(last_branch_index)[:last_displacement_index:-1]
+            last_branch_index -= 1
+            while last_branch_index > current_branch:
+                reference_update += map3D.get_vessel(last_branch_index)[::-1]
+                last_branch_index -= 1
+            for index in range(len(map3D.get_vessel(current_branch)) - 1, current_displacement-1, -1):
+                if 0 < index:
+                    reference_update.append(map3D.get_vessel(current_branch)[index])
+
+        # backward motion with branch switch
+        else:
+            raise ValueError("Different direction of motion in branch index and displacement index occured "
+                             "- which should not be possible")
+
+        return reference_update
+
+
+if __name__ == "__main__":
+    # TODO: extensive testing of 3D particle reference retriever
+    pass
