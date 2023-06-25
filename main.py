ba8e0f20b3e83b76887add74e47b4f7171557c0d
Christian Frieß <Ch.Friess@campus.lmu.de>
2023-06-25 20:20:59 +0200

    prepared performance evaluator: now ready to run!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import math
+import pickle
 import time
 from statistics import mean
 import numpy as np
@@ -149,78 +150,136 @@ def posthoc_run_3D_vessel_navigator(ref_path: str,
     logging.info("Mean particle dispersion at each time step as sem of dominant cluster = " + str(sem(err)))
 
 
-def evaluate_performance(ref_path: str,
-                         imp_path: str,
-                         grtruth_path: str,
-                         displace_path: str,
-                         dest_path: str,
-                         filename: str):
-    displacements = np.load(displace_path)
-    impedance = np.load(imp_path)
-    groundtruth = np.load(grtruth_path)
+def evaluate_performance():
+    performance_per_sample = {}
+    error_per_sample = {}
 
-    navigator = VesselNavigator()
+    particle_numbers = [100, 200]
 
-    performance = {}
-    errors = {}
-    for n in range(100, 1100, 100):
-        navigator.setup_navigator(reference_path=ref_path,
-                                  log_destination_path=dest_path,
-                                  filename=filename,
-                                  number_of_particles=1000,
-                                  initial_position_variance=0.1,
-                                  alpha_center=2,
-                                  alpha_variance=0.1,
-                                  measurement_type=MeasurementType.AHISTORIC,
-                                  injector_type=InjectorType.RANDOM_PARTICLE
-                                  )
-        acc = 0
-        clusters = []
-        for i in range(len(displacements)):
-            start = time.time()
-            clusters.append(navigator.update_step(displacement=displacements[i], impedance=impedance[i]))
-            end = time.time()
-            acc = end - start
-        estimates = [x.get_first_cluster_mean() for x in clusters]
-        performance[n] = acc / len(displacements)
-        errors[n] = VesselNavigator.rms(estimates, list(groundtruth[1:]))
+    SAMPLES = ["20", "25", "30", "31", "34"]
+    for sample_nr in SAMPLES:
+        reference_path = "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom_data_testing\\" + "smoothed_standardised_simulated_reference_agar.json"
+        impedance_path = "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom_data_testing\\sample_" + sample_nr \
+                         + "\\data_sample_" + sample_nr + "\\normalized_impedance_from_iliaca_without_plastic.npy"
+        groundtruth_path = "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom_data_testing\\sample_" + sample_nr + \
+                           "\\data_sample_" + sample_nr + "\\groundtruth_from_iliaca_without_plastic_offset_corrected.npy"
+        displacement_path = "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom_data_testing\\sample_" + sample_nr \
+                            + "\\data_sample_" + sample_nr + "\\displacements_from_iliaca_without_plastic.npy"
+        destination_path = "C:\\Users\\Chris\\OneDrive\\Desktop\\test\\performance\\AHISTORIC\\"
 
-    plt.plot(errors.keys(), errors.values())
-    plt.figure(1)
-    plt.plot(performance.keys(), performance.values())
-    np.save(dest_path + "errors per particle number", errors)
-    np.save(dest_path + "performance per particle number", performance)
+        displacements = np.load(displacement_path)
+        impedance = np.load(impedance_path)
+        groundtruth = np.load(groundtruth_path)
+
+        navigator = VesselNavigator()
+
+        performance = {}
+        errors = {}
+        for n in particle_numbers:
+            navigator.setup_navigator(reference_path=reference_path,
+                                      log_destination_path=destination_path,
+                                      filename="performance",
+                                      number_of_particles=n,
+                                      initial_position_variance=0.1,
+                                      alpha_center=2,
+                                      alpha_variance=0.1,
+                                      measurement_type=MeasurementType.AHISTORIC,
+                                      injector_type=InjectorType.RANDOM_PARTICLE
+                                      )
+            acc = 0
+            clusters = []
+            for i in range(len(displacements)):
+                start = time.time()
+                clusters.append(navigator.update_step(displacement=displacements[i], impedance=impedance[i]))
+                end = time.time()
+                acc = end - start
+            estimates = [x.get_first_cluster_mean() for x in clusters]
+            performance[n] = acc / len(displacements)
+            estimates = [x for [x, y] in estimates]
+            errors[n] = VesselNavigator.rms(signal=estimates, groundtruth=list(groundtruth[1:]))
+        performance_per_sample[sample_nr] = performance
+        error_per_sample[sample_nr] = errors
+
+    average_performance = {}
+    average_error = {}
+
+    for n in particle_numbers:
+        acc_performance = 0
+        acc_errors = 0
+        for sample_nr in SAMPLES:
+            acc_performance += performance_per_sample[sample_nr][n]
+            acc_errors += error_per_sample[sample_nr][n]
+        average_performance[n] = acc_performance / len(SAMPLES)
+        average_error[n] = acc_errors / len(SAMPLES)
+
+    fig, ax = plt.subplots()
+    ax.scatter(average_performance.keys(), average_performance.values(),  color="blue")
+    ax.plot(average_performance.keys(), average_performance.values(), color="blue", label="performance")
+    ax.set_ylabel("average time per execution step [s]")
+    ax.set_xlabel("number of particles")
+    plt.legend()
+
+    ax2 = ax.twinx()
+    ax2.scatter(average_performance.keys(), average_error.values(), color="red")
+    ax2.plot(average_performance.keys(), average_error.values(), color="red", label="errors", )
+    ax2.set_ylabel("average rms error from grountruth [mm]")
+
+    plt.legend()
+
+    destination_path = "C:\\Users\\Chris\\OneDrive\\Desktop\\test\\performance\\AHISTORIC\\"
+    plt.savefig(destination_path + "performance.svg")
+
+
+    with open(destination_path + "performance per sample.pkl", 'wb') as f:
+        pickle.dump(performance_per_sample, f)
+
+    with open(destination_path + "error per sample.pkl", 'wb') as f:
+        pickle.dump(error_per_sample, f)
+
+    with open(destination_path + "errors per particle number.pkl", 'wb') as f:
+        pickle.dump(average_error, f)
+
+    with open(destination_path + "performance per particle number.pkl", 'wb') as f:
+        pickle.dump(average_performance, f)
 
 
 if __name__ == "__main__":
+    evaluate_performance()
+    """
+    
     #SAMPLES = ["20", "25", "27", "29", "30", "31", "34", "35"]
-    SAMPLES = ["20"]
-    PATH = "C:\\Users\\Chris\\OneDrive\\Desktop\\test\\"
+    #SAMPLES = ["20", "25", "30", "31", "34"]
+    #SAMPLES = ["30", "31", "34"]
+
+    SAMPLES = ["39", "41", "42", "44", "45"]
+    #SAMPLES = ["39"]
+    PATH = "C:\\Users\\Chris\\OneDrive\\Desktop\\test_plastic\\"
 
 
     for sample_nr in SAMPLES:
         for measurement_model in MeasurementType:
 
-            reference_path = "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom_data_testing\\" + "smoothed_simulated_reference_agar.json"
+
+            reference_path = "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom_data_testing\\" + "smoothed_standardised_simulated_reference_agar.json"
             impedance_path = "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom_data_testing\\sample_" + sample_nr \
-                             + "\\data_sample_" + sample_nr + "\\impedance_from_iliaca.npy"
+                             + "\\data_sample_" + sample_nr + "\\normalized_impedance_from_iliaca_without_plastic.npy"
             groundtruth_path = "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom_data_testing\\sample_" + sample_nr + \
-                               "\\data_sample_" + sample_nr + "\\groundtruth_from_iliaca.npy"
+                               "\\data_sample_" + sample_nr + "\\groundtruth_from_iliaca_without_plastic_offset_corrected.npy"
             displacement_path = "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom_data_testing\\sample_" + sample_nr \
-                                + "\\data_sample_" + sample_nr + "\\displacements_from_iliaca.npy"
+                                + "\\data_sample_" + sample_nr + "\\displacements_from_iliaca_without_plastic.npy"
             destination_path = "C:\\Users\\Chris\\OneDrive\\Desktop\\test\\sample_" + sample_nr + "\\" + str(
                 measurement_model.name) + "\\"
-            """
-            reference_path = "C:\\Users\\Chris\\OneDrive\\Desktop\\plastic coregistration data\\04_06_2023_BS\\map_plastic.json"
-            impedance_path = "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom_data_testing\\sample_" + sample_nr \
-                             + "\\data_sample_" + sample_nr + "\\impedance_from_iliaca.npy"
-            groundtruth_path = "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom_data_testing\\sample_" + sample_nr + \
-                               "\\data_sample_" + sample_nr + "\\groundtruth_from_iliaca.npy"
-            displacement_path = "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom_data_testing\\sample_" + sample_nr \
-                                + "\\data_sample_" + sample_nr + "\\displacements_from_iliaca.npy"
+
+            reference_path = "C:\\Users\\Chris\\OneDrive\\Desktop\\plastic coregistration data\\04_06_2023_BS\\smoothed_standardised_simulated_reference_plastic.json"
+            impedance_path = "C:\\Users\\Chris\\OneDrive\\Desktop\\plastic coregistration data\\04_06_2023_BS\\coregistration_" + sample_nr \
+                             + "\\data_bioelectric_sensors" "\\normalized_impedance_from_iliaca_without_plastic.npy"
+            groundtruth_path = "C:\\Users\\Chris\\OneDrive\\Desktop\\plastic coregistration data\\04_06_2023_BS\\coregistration_" + sample_nr \
+                               + "\\data_bioelectric_sensors" "\\groundtruth_from_iliaca_without_plastic_offset_corrected.npy"
+            displacement_path = "C:\\Users\\Chris\\OneDrive\\Desktop\\plastic coregistration data\\04_06_2023_BS\\coregistration_" + sample_nr \
+                                + "\\data_bioelectric_sensors" "\\displacements_from_iliaca_without_plastic.npy"
             destination_path = "C:\\Users\\Chris\\OneDrive\\Desktop\\test_plastic\\sample_" + sample_nr + "\\" + str(
                 measurement_model.name) + "\\"
-            """
+
 
             groundtruth = np.load(groundtruth_path)
             posthoc_run_3D_vessel_navigator(ref_path=reference_path,
@@ -238,13 +297,13 @@ if __name__ == "__main__":
             exec(open("C:\\Users\\Chris\\PycharmProjects\\data_analysis_scripts_BA\\plot_result_figures.py").read(),
                  {"REF_PATH": reference_path, "IMP_PATH": impedance_path,
                   "GRTRUTH_PATH": groundtruth_path, "BASE_PATH": destination_path})
-    """
+
+
+
     exec(open("C:\\Users\\Chris\\PycharmProjects\\data_analysis_scripts_BA\\prepare_for_statistics.py").read(),
          {"SAMPLES": SAMPLES, "PATH": PATH})
 
     exec(open("C:\\Users\\Chris\\PycharmProjects\\data_analysis_scripts_BA\\statistical_evaluation.py").read(),
          {"SAMPLES": SAMPLES, "PATH": PATH})
+
     """
-
-
-
